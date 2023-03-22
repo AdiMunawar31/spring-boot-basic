125a332c92f76804f1da2abeb02bfd411552abe2
AdiMunawar31 <munawar.adi31@gmail.com>
2023-03-22 21:01:13 +0700

    create getter setter and constructor in Hero model

--- src/main/java/com/d2ycode/helloworld/models/Hero.java
+++ src/main/java/com/d2ycode/helloworld/models/Hero.java
@@ -6,4 +6,39 @@ public class Hero {
   private int id;
   private String name;
   private int demage;
+
+  public Hero(int id, String name, int demage) {
+    this.id = id;
+    this.name = name;
+    this.demage = demage;
+  }
+
+  public Hero() {
+
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getDemage() {
+    return demage;
+  }
+
+  public void setDemage(int demage) {
+    this.demage = demage;
+  }
+
 }
