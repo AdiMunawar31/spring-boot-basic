8b0234a21430161158be1cd48d638a9c9523eac7
AdiMunawar31 <munawar.adi31@gmail.com>
2023-03-23 21:24:43 +0700

    Data annotation implemented

--- src/main/java/com/d2ycode/helloworld/models/Hero.java
+++ src/main/java/com/d2ycode/helloworld/models/Hero.java
@@ -1,12 +1,10 @@
 package com.d2ycode.helloworld.models;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Hero {
