c28ec8f932f6aeaa5fcf02eb83883baae9dc658e
AdiMunawar31 <munawar.adi31@gmail.com>
2023-03-23 20:14:57 +0700

    add lombok dependency

--- src/main/java/com/d2ycode/helloworld/models/Hero.java
+++ src/main/java/com/d2ycode/helloworld/models/Hero.java
@@ -1,5 +1,14 @@
 package com.d2ycode.helloworld.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Hero {
 
   // unique identifier
@@ -7,44 +16,4 @@ public class Hero {
   private String name;
   private int demage;
   private Boolean isNewHero;
-
-  public Hero(int id, String name, int demage, Boolean isNewHero) {
-    this.id = id;
-    this.name = name;
-    this.demage = demage;
-    this.isNewHero = isNewHero;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getDemage() {
-    return demage;
-  }
-
-  public void setDemage(int demage) {
-    this.demage = demage;
-  }
-
-  public Boolean getIsNewHero() {
-    return isNewHero;
-  }
-
-  public void setIsNewHero(Boolean isNewHero) {
-    this.isNewHero = isNewHero;
-  }
-
 }
